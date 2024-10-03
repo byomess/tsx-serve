9b1bbfc9f89fd8800b3fb6ba6d5628210adda120
Felipe Chierice <felipe.chierice@hotmail.com>
2024-10-03 16:09:39 -0300

    chore: minor code improvements

--- src/tsx-serve.ts
+++ src/tsx-serve.ts
@@ -4,19 +4,24 @@ import fs from 'fs';
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 
 // Define types for supported tunnels and arguments
-type Tunnel = { long: string; short: string };
-type ArgMapItem = { 
-    long: string; 
-    short: string; 
-    type?: 'string' | 'number' | 'boolean'; 
-    values?: Tunnel[]; 
-    description: string; 
+type Tunnel = {
+    long: string;
+    short: string;
+    quickSubdomains?: boolean
+};
+
+type ArgMapItem = {
+    long: string;
+    short: string;
+    type?: 'string' | 'number' | 'boolean';
+    values?: Tunnel[];
+    description: string;
     default?: number;
 };
 
 // Supported tunnel services
 const SUPPORTED_TUNNELS: Tunnel[] = [
-    { long: 'localtunnel', short: 'lt' },
+    { long: 'localtunnel', short: 'lt', quickSubdomains: true },
     { long: 'pinggy', short: 'pg' },
 ];
 
@@ -37,7 +42,7 @@ const argMap: ArgMapItem[] = [
         description: 'Local tunnel service',
     },
     {
-        long: '--tunnel-subdomain',
+        long: '--subdomain',
         short: '-s',
         type: 'string',
         description: 'Subdomain for the tunnel',
@@ -57,7 +62,6 @@ const argMap: ArgMapItem[] = [
 // Parse CLI arguments
 const parsedArgs = argsParser(process.argv.slice(2), argMap);
 
-console.log('Parsed args:', parsedArgs);
 // Function to parse and map arguments
 function argsParser(args: string[], argMap: ArgMapItem[]) {
     return args.reduce((acc: Record<string, any>, arg, index) => {
@@ -103,94 +107,36 @@ const path = parsedArgs['path'] || '.';
 validatePath(path);
 
 // Find the tunnel configuration
-const tunnelConfig = SUPPORTED_TUNNELS.find(t => t.long === parsedArgs['--tunnel'] || t.short === parsedArgs['--tunnel']);
+let tunnelConfig = SUPPORTED_TUNNELS.find(t => t.long === parsedArgs['--tunnel'] || t.short === parsedArgs['--tunnel']);
 
-if (tunnelConfig && !SUPPORTED_TUNNELS.find(t => t.long === tunnelConfig.long)) {
-    console.error(`Unsupported tunnel service: ${tunnelConfig.long}`);
-    process.exit(1);
+// If subdomain is specified but tunnel option is not, default to localtunnel
+if (!tunnelConfig) {
+    if (parsedArgs['--subdomain']) {
+        parsedArgs['--tunnel'] = 'localtunnel';
+        tunnelConfig = SUPPORTED_TUNNELS.find(t => t.long === 'localtunnel');
+    } else if (parsedArgs['--tunnel']) {
+        console.error(`Unsupported tunnel service: ${parsedArgs['--tunnel']}`);
+        console.error(`These are the supported tunnel services: ${SUPPORTED_TUNNELS.map(t => t.long).join(', ')}`);
+        process.exit(1);
+    }
 }
 
-const app = express();
-setupExpress(app, path);
-
-app.listen(port, async () => {
-    console.log(`\nServing path: \x1b[1m${path}\x1b[0m`);
-    console.log(`\nLocal server listening on \x1b[1mhttp://localhost:${port}\x1b[0m\n`);
-
-    if (tunnelConfig) {
-        try {
-            switch (tunnelConfig.long) {
-                case 'localtunnel': // Localtunnel
-                    console.log('Creating localtunnel...');
-                    const tunnelInstance = await lt({ port, subdomain: parsedArgs['--tunnel-subdomain'] });
-                    console.log(`Tunnel started on ${tunnelInstance.url}\n`);
-                    waitToExit();
-                    break;
-
-                case 'pinggy': // Pinggy
-                    console.log('Creating Pinggy tunnel...\n');
-
-                    const pinggyTunnel = spawn('ssh', ['-T', '-p', '443', `-R0:localhost:${port}`, '-L4300:localhost:4300', 'qr@a.pinggy.io'], {
-                        stdio: ['pipe', 'pipe', 'pipe'],
-                    });
-
-                    pinggyTunnel.stdout.on('data', (data) => {
-                        const lines = data.toString().split('\n');
-                        const tunnelHttpUrl = lines.find(l => /^http:\/\/.*\.free\.pinggy\.lin/.test(l))?.trim();
-                        const tunnelHttpsUrl = lines.find(l => /^https:\/\/.*\.free\.pinggy\.lin/.test(l))?.trim();
-
-                        if (tunnelHttpUrl) console.log('Http URL:\t\x1b[1m', tunnelHttpUrl, '\x1b[0m');
-                        if (tunnelHttpsUrl) {
-                            console.log('Https URL:\t\x1b[1m', tunnelHttpsUrl, '\x1b[0m\n');
-                            waitToExit(pinggyTunnel);
-                        }
-                    });
-
-                    pinggyTunnel.stderr.on('data', (data) => {
-                        const message = data.toString();
-                        if (!message.includes('Allocated port')) {
-                            console.error('Pinggy error:', message);
-                        }
-                    });
-
-                    pinggyTunnel.on('close', (code) => {
-                        console.log('Pinggy tunnel closed with code:', code);
-                        process.exit(code === 0 || code === 255 ? 0 : 1);
-                    });
-
-                    pinggyTunnel.on('exit', (code) => {
-                        console.log('Pinggy tunnel exited with code:', code);
-                        process.exit(code === 0 || code === 255 ? 0 : 1);
-                    });
-
-                    break;
-
-                default:
-                    console.error(`Unsupported tunnel service: ${tunnelConfig.long}`);
-                    process.exit(1);
-            }
-        } catch (err) {
-            console.error('Error creating tunnel:', err);
-            process.exit(1);
-        }
-    } else {
-        waitToExit();
-    }
-});
+// Check if selected tunneling service supports quick subdomains
+if (parsedArgs['--subdomain'] && tunnelConfig && !tunnelConfig.quickSubdomains) {
+    console.error(`Subdomains are not supported by ${tunnelConfig.long}`);
+    console.error(`These are the supported tunnel services with subdomains: ${SUPPORTED_TUNNELS.filter(t => t.quickSubdomains).map(t => t.long).join(', ')}`);
+    process.exit(1);
+}
 
 // Function to create tunnels based on the selected service
 async function createTunnel(service: string, port: number) {
     try {
         switch (service) {
             case 'localtunnel':
-                console.log('Creating localtunnel...\n');
-                const tunnelInstance = await lt({ port });
-                console.log(`Tunnel started on ${tunnelInstance.url}\n`);
-                waitToExit();
+                createLocalTunnel();
                 break;
             case 'pinggy':
-                console.log('Creating \x1b[1mPinggy\x1b[0m tunnel...\n');
-                createPinggyTunnel(port);
+                createPinggyTunnel();
                 break;
         }
     } catch (err) {
@@ -199,31 +145,51 @@ async function createTunnel(service: string, port: number) {
     }
 }
 
+// Function to create localtunnel
+async function createLocalTunnel() {
+    console.log('Creating \x1b[1mlocaltunnel\x1b[0m tunnel...\n');
+    const ltConfig = { port };
+    if (parsedArgs['--subdomain']) ltConfig['subdomain'] = parsedArgs['--subdomain'];
+    const tunnelInstance = await lt(ltConfig);
+    console.log(`Tunnel started on ${tunnelInstance.url}\n`);
+    waitToExit();
+}
+
 // Function to create Pinggy tunnel
-function createPinggyTunnel(port: number) {
-    const pinggyTunnel = spawn('ssh', ['-T', '-p', '443', `-R0:localhost:${port}`, '-L4300:localhost:4300', 'qr@a.pinggy.io']);
+function createPinggyTunnel() {
+    console.log('Creating \x1b[1mPinggy\x1b[0m tunnel...\n');
+
+    const pinggyTunnel = spawn('ssh', ['-T', '-p', '443', `-R0:localhost:${port}`, '-L4300:localhost:4300', 'qr@a.pinggy.io'], {
+        stdio: ['pipe', 'pipe', 'pipe'],
+    });
 
     pinggyTunnel.stdout.on('data', (data) => {
         const lines = data.toString().split('\n');
-        const tunnelUrl = lines.find(l => l.includes('.free.pinggy.lin'))?.trim();
-        if (tunnelUrl) {
-            console.log('Tunnel URL:\t\x1b[1m', tunnelUrl, '\x1b[0m\n');
+        const tunnelHttpUrl = lines.find(l => /^http:\/\/.*\.free\.pinggy\.lin/.test(l))?.trim();
+        const tunnelHttpsUrl = lines.find(l => /^https:\/\/.*\.free\.pinggy\.lin/.test(l))?.trim();
+
+        if (tunnelHttpUrl) console.log('Http URL:\t\x1b[1m', tunnelHttpUrl, '\x1b[0m');
+        if (tunnelHttpsUrl) {
+            console.log('Https URL:\t\x1b[1m', tunnelHttpsUrl, '\x1b[0m\n');
             waitToExit(pinggyTunnel);
         }
     });
 
     pinggyTunnel.stderr.on('data', (data) => {
-        console.error('Pinggy error:', data.toString());
+        const message = data.toString();
+        if (!message.includes('Allocated port')) {
+            console.error('Pinggy error:', message);
+        }
     });
 
     pinggyTunnel.on('close', (code) => {
         console.log('Pinggy tunnel closed with code:', code);
-        process.exit(code ?? 1);
+        process.exit(code === 0 || code === 255 ? 0 : 1);
     });
 
     pinggyTunnel.on('exit', (code) => {
         console.log('Pinggy tunnel exited with code:', code);
-        process.exit(code ?? 1);
+        process.exit(code === 0 || code === 255 ? 0 : 1);
     });
 }
 
@@ -305,3 +271,17 @@ function setupExpress(app: express.Application, path: string) {
         app.use(express.static(path));
     }
 }
+
+const app = express();
+setupExpress(app, path);
+
+app.listen(port, async () => {
+    console.log(`\nServing path: \x1b[1m${path}\x1b[0m`);
+    console.log(`\nLocal server listening on \x1b[1mhttp://localhost:${port}\x1b[0m\n`);
+
+    if (tunnelConfig) {
+        createTunnel(tunnelConfig.long, port);
+    } else {
+        waitToExit();
+    }
+});
